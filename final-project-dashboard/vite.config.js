d59e5eded175d72c8fcbc933d173f28853cb93fe
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-06-16 08:43:58 +0530

    updated

--- final-project-dashboard/vite.config.js
+++ final-project-dashboard/vite.config.js
@@ -1,13 +1,5 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // base: '/YOUR_BASE/'
-  //   build: {
-  //     rollupOptions: {
-  //       input: {
-  //         // main: resolve(__dirname, "index.html"),
-  //       },
-  //     },
-  //   },
-});
+  base: "/agent-registration/"
+})

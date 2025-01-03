b19882cc4c955a4b6558262515a8992b2b37ff93
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2025-01-03 15:59:25 +0530

    feat: task 2

---
+++ Flutter-Learning/task_02/lib/common/cusText.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// reusable custom text widget
+Widget CusText({
+  required String text,
+  Color? bgColor,
+  double? fontSize = 16.0,
+}) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 10.0),
+    child: DecoratedBox(
+      decoration: BoxDecoration(
+        color: bgColor,
+        borderRadius: BorderRadius.circular(3.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ),
+  );
+}
--- Flutter-Learning/task_02/lib/main.dart
+++ Flutter-Learning/task_02/lib/main.dart
@@ -1,18 +1,145 @@
 import 'package:flutter/material.dart';
+import 'package:task_02/common/cusText.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment
+              .center, // what this line does is it aligns the container in the center of the screen
+          children: [
+            Center(
+              child: Container(
+                width: 350,
+                height: 800,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 120,
+                            height: 127,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: CusText(text: 'Image 1'),
+                            ),
+                          ),
+                          Expanded(
+                              child: Container(
+                            height: 127,
+                            margin: const EdgeInsets.only(left: 5),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.zero,
+                                  child: CusText(text: 'Apple'),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.zero,
+                                  child: CusText(text: 'This is an apple'),
+                                ),
+                              ],
+                            ),
+                          ))
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ), // this act like mt a.k.a margin-top
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            width: 120,
+                            height: 127,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Align(
+                              alignment: Alignment.center,
+                              child: CusText(text: 'Image 2'),
+                            ),
+                          ),
+                          Expanded(
+                              child: Container(
+                            height: 127,
+                            margin: const EdgeInsets.only(left: 5),
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.zero,
+                                  child: CusText(text: 'Orange'),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.zero,
+                                  child: CusText(text: 'This is an orange'),
+                                ),
+                              ],
+                            ),
+                          ))
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Expanded(
+                              child: Container(
+                            height: 127,
+                            decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(5),
+                            ),
+                            child: Column(
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.zero,
+                                  child: CusText(text: 'Apple'),
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.zero,
+                                  child: CusText(text: 'Apple is a healthier'),
+                                ),
+                              ],
+                            ),
+                          ))
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
